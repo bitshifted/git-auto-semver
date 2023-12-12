e706d0b32f45698f36b2926a35a679a075b80eb8
Vladimir Djurovic <vdjurovic@bitshifted.co>
2023-12-12 18:17:17 +0100

    update version calculation

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,11 +3,19 @@
 
 set -u # gives warning when using undeclared variables
 
+VERSION_STRING="1.0.0"
+
 echo "Github event name: $GITHUB_EVENT_NAME"
 echo "Github event path:  $GITHUB_EVENT_PATH"
 
 cat $GITHUB_EVENT_PATH
 
-echo "{version-string}={1.2.3}" >> "$GITHUB_OUTPUT"
+
+if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+    VERSION_STRING=$(version.sh --pull-request)
+fi
+
+echo "Calculated version: $VERSION_STRING"
+echo "version-string=$VERSION_STRING" >> "$GITHUB_OUTPUT"
 
 exit 0
