bdb2a3698fb4b49c3806389cb3e165b8436a5137
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2023-12-12 19:25:11 +0100

    feat: Initial implementation (#2)
    Co-authored-by: Vladimir Djurovic <vdjurovic@bitshifted.co>

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,11 @@ LABEL "com.github.actions.name" = "Git Automatic Semantic Versioning"
 LABEL "com.github.actions.description"="Automatically create new semantci version string based on commit message"
 
 
-LABEL version=$IMAGE_VERSION
 LABEL repository="https://github.com/bitshifted/git-auto-semver"
 LABEL maintainer="Bitshift"
 
-COPY scripts/entrypoint.sh /usr/bin
-RUN chmod 755 /usr/bin/entrypoint.sh
-RUN apt update && apt install -y git jq
+COPY scripts/* /usr/bin
+RUN chmod 755 /usr/bin/entrypoint.sh && chmod 755 /usr/bin/version.sh && chmod 755 /usr/bin/semver-bump.sh
+RUN apt update && apt install -y git
 
 ENTRYPOINT ["/usr/bin/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,11 +3,25 @@
 
 set -u # gives warning when using undeclared variables
 
+# fix error 'fatal: detected dubious ownership in repository at '/github/workspace'' in Github Action
+sh -c "git config --global --add safe.directory $PWD"
+
+echo "Initial version: ${INPUT_INITIAL_VERSION}"
+VERSION_STRING="1.0.0"
+
 echo "Github event name: $GITHUB_EVENT_NAME"
 echo "Github event path:  $GITHUB_EVENT_PATH"
 
-cat $GITHUB_EVENT_NAME
+cat $GITHUB_EVENT_PATH
 
-echo "::set-output name=version-string::1.2.3"
+
+if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+    VERSION_STRING=$(version.sh --pull-request)
+elif [ "$GITHUB_EVENT_NAME" = "push" ]; then
+    VERSION_STRING=$(version.sh)
+fi
+
+echo "Calculated version: $VERSION_STRING"
+echo "version-string=$VERSION_STRING" >> "$GITHUB_OUTPUT"
 
 exit 0
