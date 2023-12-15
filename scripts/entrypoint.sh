c17630ba87d9e096b0500d252f6726f4d2dfd74a
Vladimir Djurovic <vdjurovic@bitshifted.co>
2023-12-15 19:18:51 +0100

    feat: create tag option

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,8 +7,6 @@ set -u # gives warning when using undeclared variables
 sh -c "git config --global --add safe.directory $PWD"
 
 VERSION_STRING="$INPUT_INITIAL_VERSION"
-echo "main branch: $INPUT_MAIN_BRANCH"
-echo "Github ref: $GITHUB_REF"
 
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
@@ -18,6 +16,12 @@ elif [ "$GITHUB_EVENT_NAME" = "push" ]; then
         VERSION_STRING=$(version.sh)
     else 
         echo "Push not on main branch"
+        exit 0
+    fi
+    if [ "$INPUT_CREATE_TAG" = "true" ]; then
+        echo "Creating tag $INPUT_TAG_PREFIX$VERSION_STRING"
+        git tag  $INPUT_TAG_PREFIX$VERSION_STRING
+        git push origin --tags
     fi
 fi
 
